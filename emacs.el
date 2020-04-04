0d38260e9a25097365d88f4dc63ea29b6b80e038
mulenatic <mulenatic@gmail.com>
2020-04-04 10:39:18 +0200

    Enabled javascript-eslint checker for web mode, changed Identation.

--- emacs.el
+++ emacs.el
@@ -55,3 +55,24 @@
 
 (when (fboundp 'winner-mode)
         (winner-mode 1))
+
+;; http://www.flycheck.org/manual/latest/index.html
+(require 'flycheck)
+;; turn on flychecking globally
+(add-hook 'after-init-hook #'global-flycheck-mode)
+;; disable json-jsonlist checking for json files
+(setq-default flycheck-disabled-checkers (append flycheck-disabled-checkers '(json-jsonlist)))
+;; disable jshint since we prefer eslint checking
+(setq-default flycheck-disabled-checkers (append flycheck-disabled-checkers '(javascript-jshint)))
+;; use eslint with web-mode for jsx files
+(flycheck-add-mode 'javascript-eslint 'web-mode)
+
+
+;; adjust indents for web-mode to 2 spaces
+(defun my-web-mode-hook ()
+  "Hooks for Web mode. Adjust indents"
+  ;;; http://web-mode.org/
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2))
+(add-hook 'web-mode-hook  'my-web-mode-hook)
